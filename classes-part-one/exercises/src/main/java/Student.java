303a58859d3af037d55fea10a66761a460d6f98d
jojotier <122423763+jojotier@users.noreply.github.com>
2023-08-17 20:47:26 -0500

    figured out how to 1) convert string into a character arraylist and 2) iterate through the arraylist to print every letter on a new line. Will attempt to figure out how to make new git branches via intellij and get the hashmap method to work from there
    FINALLY figured out the hashmap bit

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -1,10 +1,46 @@
 public class Student {
+
         private String name;
         private int studentId;
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
+
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+        public String getName(){
+                return name;
+        }
+        public void setName(String name){
+                this.name = name;
+        }
+        public void setStudentId(int studentId){
+                this.studentId = studentId;
+        }
+        private void setNumberOfCredits(int numberOfCredits){
+                this.numberOfCredits = numberOfCredits;
+        }
+        public void setGpa(double gpa){
+                this.gpa = gpa;
+        }
+        public String getName(String name){
+                return name;
+        }
+        public int getStudentId(int studentId){
+                return studentId;
+        }
+        public int getNumberOfCredits(int numberOfCredits){
+                return numberOfCredits;
+        }
+        public double getGpa(double gpa){
+                return gpa;
+        }
+
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
 
 }
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,6 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student newStudent = new Student("Sam", 123, 1, 4.0);
     }
 }
